3829e8b6ef40099d1b5af2d549840df0ca60f0ba
misraved <ved@turbot.com>
2023-12-12 19:23:53 +0530

    Update connection config variable definition to use the latest hcl syntax

--- hubspot/connection_config.go
+++ hubspot/connection_config.go
@@ -2,17 +2,10 @@ package hubspot
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type hubSpotConfig struct {
-	PrivateAppToken *string `cty:"private_app_token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"private_app_token": {
-		Type: schema.TypeString,
-	},
+	PrivateAppToken *string `hcl:"private_app_token"`
 }
 
 func ConfigInstance() interface{} {
